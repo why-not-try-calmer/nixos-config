e3a71fbc382f8ef0691654d56221bf7c3c8439ed
why-not-calmer <mrnycticorax@gmail.com>
2021-06-04 12:32:05 +0200

    ---

--- configuration.nix
+++ configuration.nix
@@ -306,3 +306,159 @@
   };
 
 }
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.useOSProber = true;
+  boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.kernelParams = ["pci=noaer splash=silent resume=/dev/sda6 rd.driver.blacklist=nouveau modprobe.blacklist=nouveau nouveau.modeset=0 quiet pci=noaer dis_ucode_ldr"];
+  
+  #network
+  networking.hostName = "nixos"; # Define your hostname.
+  networking.networkmanager.enable = true;
+ 
+   # Set your time zone.
+  time.timeZone = "Europe/Paris";
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.enp3s0f1.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # module
+  hardware.bluetooth.enable = true;
+  
+  # Select internationalisation properties.
+  i18n.defaultLocale = "fr_CH.UTF-8";
+  console = {
+    font = "Lat2-Terminus16";
+    keyMap = "fr_CH-latin1";
+  };
+
+  # Fonts
+  fonts.fonts = with pkgs; [
+    fira-code
+    fira-code-symbols
+  ];
+
+  # Enable the Plasma 5 Desktop Environment.
+  services.xserver.enable = true;
+  services.xserver.videoDrivers = [ "intel" ];
+  services.xserver.useGlamor = true;
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+  # services.xserver.desktopManager.plasma5.runUsingSystemd = true;
+  # services.xserver.displayManager.sessionPackages = [
+  # (pkgs.plasma-workspace.overrideAttrs
+  #  (old: { passthru.providedSessions = [ "plasmawayland" ]; }))
+  # ];
+  
+  # gnome keyring
+  services.gnome.gnome-keyring.enable = true;
+
+  # Configure keymap in X11
+  services.xserver.layout = "us";
+  services.xserver.xkbOptions = "eurosign:e";
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound.
+  sound.enable = true;
+  hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  services.xserver.libinput.enable = true;
+
+  # Flatpak
+  services.flatpak.enable = true;
+  xdg.portal.extraPortals = [ pkgs.xdg-desktop-portal-gtk ];
+  
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hades = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "networkmanager" ]; # Enable ‘sudo’ for the user.
+    shell = pkgs.fish;
+  };
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  nixpkgs.config.allowUnfree = true;
+  environment.systemPackages = with pkgs; [
+    wget vim
+    partition-manager
+    nano
+    fish
+    yakuake
+    git
+    ncdu
+    hlint
+    stack
+    # kate moved to flatpak for better dictionary support
+    ark
+    vscode
+    nodejs
+    thunderbird
+    brave
+    kontact
+    korganizer
+    kcalc
+    gwenview
+    okular
+    tdesktop
+  ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  programs.mtr.enable = true; 
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+  };
+  programs.fish.enable = true;
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.09"; # Did you read the comment?
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+}
+
